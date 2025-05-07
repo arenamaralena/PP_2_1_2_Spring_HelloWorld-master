31cf92edb8f61c59915636cf76e5daf331087ea7
Arina M <arenamaralena@yandex.ru>
2025-05-07 13:34:46 +0300

    update Cat class

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,2 +1,15 @@
 public class Cat {
+    private String cat;
+
+    public Cat() {
+
+    }
+
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
 }
